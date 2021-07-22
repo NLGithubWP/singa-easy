6b41d07517bb701db7599053d1aaaa9778acc6db
nailixing <xingnaili14@gmail.com>
2021-07-22 11:27:02 +0800

    update col num

--- singa_easy/modules/mod_modelslicing/inference.py
+++ singa_easy/modules/mod_modelslicing/inference.py
@@ -206,85 +206,6 @@ if not os.path.isdir('log/{}'.format(args.exp_name)):
 # load dataset
 train_loader, val_loader, args.class_num = data_loader(args)
 
-
-def main():
-    global args, best_err1, best_err5
-    print_logger = logger(args.log_path, True, True)
-    print(vars(args))
-
-    # create model and upgrade model to support model slicing
-    model = create_model(args, print_logger)
-    model = upgrade_dynamic_layers(model, args.groups, args.sr_list)
-    model = torch.nn.DataParallel(model).cuda()
-    criterion = torch.nn.CrossEntropyLoss().cuda()
-    optimizer = torch.optim.SGD(model.parameters(),
-                                args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay,
-                                nesterov=True)
-
-    if args.resume:
-        checkpoint = load_checkpoint(print_logger)
-        epoch, best_err1, best_err5, model_state, optimizer_state, scheduler_state = checkpoint.values()
-        model.load_state_dict(model_state)
-        optimizer.load_state_dict(optimizer_state)
-        print("==> finish loading checkpoint '{}' (epoch {})".format(args.resume, epoch))
-    cudnn.benchmark = True
-
-    # evaluate on all the sr_idxs, from the smallest subnet to the largest
-    starter = torch.cuda.Event(enable_timing=True)
-    ender = torch.cuda.Event(enable_timing=True)
-
-    # warmup
-    for sr_idx in reversed(range(len(args.sr_list))):
-        args.sr_idx = sr_idx
-        model.module.update_sr_idx(sr_idx)
-        for idx, (input, target) in enumerate(val_loader):
-            print("GPU-WARM-UP batchid", idx, "Under slice rate ", args.sr_list[sr_idx], "---" * 5)
-            if torch.cuda.is_available():
-                input = input.cuda(non_blocking=True)
-                target.cuda(non_blocking=True)
-            model(input)
-            torch.cuda.synchronize()
-            break
-
-    print("GPU-WARM-UP done")
-    result = []
-    fo = open(args.predicted_save_file + ".txt", "a+")
-
-    fo.write("When num_img/batch_size=" + str(args.predict_batch_nums) + "\n")
-
-    for sr_idx in reversed(range(len(args.sr_list))):
-        args.sr_idx = sr_idx
-        print("Begin", "---" * 20)
-        print("Under slice rate ", args.sr_list[sr_idx], "---" * 5)
-        model.module.update_sr_idx(sr_idx)
-        correct_k, num_img, total_time, num_batch = test_1_batch_examples(starter, ender, model)
-        result.append([correct_k, num_img, total_time, args.sr_list[sr_idx], num_batch])
-
-    for ele in result:
-        correct_k, num_img, total_time, sr_idx, num_batch = ele[0], ele[1], ele[2], ele[3], ele[4]
-        print("sr_idx=", sr_idx, " correct_k", correct_k)
-        print("sr_idx=", sr_idx, " num_img", num_img)
-        print("sr_idx=", sr_idx, " accuracy", correct_k / num_img)
-        print("sr_idx=", sr_idx, " average_time", total_time / num_img)
-        print("sr_idx=", sr_idx, " throughput", num_img / total_time)
-        print("sr_idx=", sr_idx, " End", "---" * 20)
-
-        fo.write("     sr_idx=" + str(sr_idx) + " num_img=" + str(num_img) + "\n")
-        fo.write("     sr_idx=" + str(sr_idx) + " correct_k=" + str(correct_k) + "\n")
-        fo.write("     sr_idx=" + str(sr_idx) + " accuracy=" + str(correct_k / num_img) + "\n")
-        fo.write("     sr_idx=" + str(sr_idx) + " average_time=" + str(total_time / num_img) + "\n")
-        fo.write("     sr_idx=" + str(sr_idx) + " total_time=" + str(total_time) + "\n")
-        fo.write("     sr_idx=" + str(sr_idx) + " throughput=" + str(num_img / total_time) + "\n")
-
-        fo.write("     sr_idx=" + str(sr_idx) + " average_batch_time=" + str(total_time / num_batch) + "\n")
-        fo.write("     sr_idx=" + str(sr_idx) + " throughput_batch=" + str(num_batch / total_time) + "\n")
-
-        fo.write("\n")
-    fo.close()
-
-
 def create_model(args, print_logger):
     print_logger.info("==> creating model '{}'".format(args.net_type))
     if args.dataset.startswith('cifar'):
@@ -352,35 +273,185 @@ def load_checkpoint(print_logger):
     return checkpoint
 
 
-def test_any_batch_examples(starter, ender, model):
+def main():
+    global args, best_err1, best_err5
+    print_logger = logger(args.log_path, True, True)
+    print(vars(args))
+
+    # create model and upgrade model to support model slicing
+    model = create_model(args, print_logger)
+    model = upgrade_dynamic_layers(model, args.groups, args.sr_list)
+    model = torch.nn.DataParallel(model).cuda()
+    criterion = torch.nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.SGD(model.parameters(),
+                                args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay,
+                                nesterov=True)
+
+    if args.resume:
+        checkpoint = load_checkpoint(print_logger)
+        epoch, best_err1, best_err5, model_state, optimizer_state, scheduler_state = checkpoint.values()
+        model.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
+        print("==> finish loading checkpoint '{}' (epoch {})".format(args.resume, epoch))
+    cudnn.benchmark = True
+
+    # evaluate on all the sr_idxs, from the smallest subnet to the largest
+    starter = torch.cuda.Event(enable_timing=True)
+    ender = torch.cuda.Event(enable_timing=True)
+
+    # warmup
+    for sr_idx in reversed(range(len(args.sr_list))):
+        args.sr_idx = sr_idx
+        model.module.update_sr_idx(sr_idx)
+        for idx, (input, target) in enumerate(val_loader):
+            print("GPU-WARM-UP batchid", idx, "Under slice rate ", args.sr_list[sr_idx], "---" * 5)
+            if torch.cuda.is_available():
+                input = input.cuda(non_blocking=True)
+                target.cuda(non_blocking=True)
+            model(input)
+            torch.cuda.synchronize()
+            break
+
+    print("GPU-WARM-UP done")
+
+    log_test_1_batch_examples(model, starter, ender)
+
+
+def test_scheduler_batch_examples(starter, ender, model):
+    scheduler_map = \
+        {32: [32, 0, 0, 0], 160: [160, 0, 0, 0], 320: [320, 0, 0, 0], 640: [640, 0, 0, 0], 960: [960, 0, 0, 0],
+         1280: [1280, 0, 0, 0], 1600: [1600, 0, 0, 0], 1920: [1920, 0, 0, 0], 2240: [2240, 0, 0, 0], 2560: [2560, 0, 0, 0],
+         3200: [3200, 0, 0, 0], 3840: [3840, 0, 0, 0], 4480: [4480, 0, 0, 0], 4800: [4800, 0, 0, 0], 5120: [5120, 0, 0, 0],
+         5440: [5440, 0, 0, 0], 5760: [5586, 0, 174, 0], 6400: [4937, 0, 1463, 0], 8000: [3314, 0, 4686, 0],
+         9600: [1691, 0, 7909, 0], 11200: [68, 1, 11131, 0], 12800: [0, 0, 7854, 4946], 17600: [0, 0, 1, 16325],
+         19200: [0, 0, 1, 16325], 20800: [0, 0, 1, 16325], 22400: [0, 0, 1, 16325], 24000: [0, 0, 1, 16325],
+         25600: [0, 0, 1, 16325], 32000: [0, 0, 1, 16325]}
+
+    sliceRateMapper = {0.25: 3,
+                       0.5: 2,
+                       0.75: 1,
+                       1.0: 0}
+
+    result = []
+    fo = open(args.predicted_save_file + ".txt", "a+")
+
+    fo.write("When num_img/batch_size=" + str(args.predict_batch_nums) + "\n")
+
+    max_images_num = args.predict_batch_nums*args.batch_size
+
+    if max_images_num in scheduler_map:
+        n1, n2, n3, n4 = scheduler_map[max_images_num]
+    else:
+        fo.write("     scheduler not support the current workload\n")
+        fo.close()
+        return
+
     correct_k = 0
     total_time = 0
-    nbatch = 1
     num_img = 0
-    for idx, (input, target) in enumerate(val_loader):
-        print("batchid", idx)
-        if torch.cuda.is_available():
-            input = input.cuda(non_blocking=True)
-            target = target.cuda(non_blocking=True)
-        starter.record()
-        output = model(input)
-        ender.record()
-        torch.cuda.synchronize()
-        curr_time = starter.elapsed_time(ender)
-        # calculate
-        total_time += curr_time
-        # print(" input.size()", len(target), target)
-        num_img += args.batch_size
-        print("image number", num_img)
-        correct_k += accuracy_float(output, target, topk=(1, 1))
-    print("correct_k", correct_k)
-    print("num_img", num_img)
-    print("accuracy", correct_k / num_img)
-    print("average_time", total_time / num_img)
-    print("End", "---" * 20)
+    num_batch = 0
+
+    if n1 != 0:
+        print("Switch to model with slice-rate=", sliceRateMapper[1.0])
+        model.module.update_sr_idx(sliceRateMapper[1.0])
+        correct_k_tmp, num_img_tmp, total_time_tmp, num_batch_tmp = test_1_batch_examples(starter, ender, model, n1)
+
+        correct_k += correct_k_tmp
+        total_time += total_time_tmp
+        num_img += num_img_tmp
+
+    if n2 != 0:
+        print("Switch to model with slice-rate=", sliceRateMapper[0.75])
+        model.module.update_sr_idx(sliceRateMapper[0.75])
+        correct_k_tmp, num_img_tmp, total_time_tmp, num_batch_tmp = test_1_batch_examples(starter, ender, model, n2)
+
+        correct_k += correct_k_tmp
+        total_time += total_time_tmp
+        num_img += num_img_tmp
+
+    if n3 != 0:
+        print("Switch to model with slice-rate=", sliceRateMapper[0.5])
+        model.module.update_sr_idx(sliceRateMapper[0.5])
+        correct_k_tmp, num_img_tmp, total_time_tmp, num_batch_tmp = test_1_batch_examples(starter, ender, model, n3)
+
+        correct_k += correct_k_tmp
+        total_time += total_time_tmp
+        num_img += num_img_tmp
+
+    if n4 != 0:
+        print("Switch to model with slice-rate=", sliceRateMapper[0.25])
+        model.module.update_sr_idx(sliceRateMapper[0.25])
+        correct_k_tmp, num_img_tmp, total_time_tmp, num_batch_tmp = test_1_batch_examples(starter, ender, model, n4)
+
+        correct_k += correct_k_tmp
+        total_time += total_time_tmp
+        num_img += num_img_tmp
+
+    num_batch = num_img/args.batch_size
+    result.append([correct_k, num_img, total_time, "scheduler", num_batch])
+
+    for ele in result:
+        correct_k, num_img, total_time, sr_idx, num_batch = ele[0], ele[1], ele[2], ele[3], ele[4]
+        print("sr_idx=", sr_idx, " correct_k", correct_k)
+        print("sr_idx=", sr_idx, " num_img", num_img)
+        print("sr_idx=", sr_idx, " accuracy", correct_k / num_img)
+        print("sr_idx=", sr_idx, " average_time", total_time / num_img)
+        print("sr_idx=", sr_idx, " throughput", num_img / total_time)
+        print("sr_idx=", sr_idx, " End", "---" * 20)
+
+        fo.write("     sr_idx=" + str(sr_idx) + " num_img=" + str(num_img) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " correct_k=" + str(correct_k) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " accuracy=" + str(correct_k / num_img) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " average_time=" + str(total_time / num_img) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " total_time=" + str(total_time) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " throughput=" + str(num_img / total_time) + "\n")
+
+        fo.write("     sr_idx=" + str(sr_idx) + " average_batch_time=" + str(total_time / num_batch) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " throughput_batch=" + str(num_batch / total_time) + "\n")
+
+        fo.write("\n")
+    fo.close()
 
 
-def test_1_batch_examples(starter, ender, model):
+def log_test_1_batch_examples(model, starter, ender):
+    result = []
+    fo = open(args.predicted_save_file + ".txt", "a+")
+
+    fo.write("When num_img/batch_size=" + str(args.predict_batch_nums) + "\n")
+
+    for sr_idx in reversed(range(len(args.sr_list))):
+        args.sr_idx = sr_idx
+        print("Begin", "---" * 20)
+        print("Under slice rate ", args.sr_list[sr_idx], "---" * 5)
+        model.module.update_sr_idx(sr_idx)
+        correct_k, num_img, total_time, num_batch = test_1_batch_examples(starter, ender, model, args.predict_batch_nums*args.batch_size)
+        result.append([correct_k, num_img, total_time, args.sr_list[sr_idx], num_batch])
+
+    for ele in result:
+        correct_k, num_img, total_time, sr_idx, num_batch = ele[0], ele[1], ele[2], ele[3], ele[4]
+        print("sr_idx=", sr_idx, " correct_k", correct_k)
+        print("sr_idx=", sr_idx, " num_img", num_img)
+        print("sr_idx=", sr_idx, " accuracy", correct_k / num_img)
+        print("sr_idx=", sr_idx, " average_time", total_time / num_img)
+        print("sr_idx=", sr_idx, " throughput", num_img / total_time)
+        print("sr_idx=", sr_idx, " End", "---" * 20)
+
+        fo.write("     sr_idx=" + str(sr_idx) + " num_img=" + str(num_img) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " correct_k=" + str(correct_k) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " accuracy=" + str(correct_k / num_img) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " average_time=" + str(total_time / num_img) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " total_time=" + str(total_time) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " throughput=" + str(num_img / total_time) + "\n")
+
+        fo.write("     sr_idx=" + str(sr_idx) + " average_batch_time=" + str(total_time / num_batch) + "\n")
+        fo.write("     sr_idx=" + str(sr_idx) + " throughput_batch=" + str(num_batch / total_time) + "\n")
+
+        fo.write("\n")
+    fo.close()
+
+def test_1_batch_examples(starter, ender, model, maximg):
     correct_k = 0
     total_time = 0
     num_img = 0
@@ -401,7 +472,7 @@ def test_1_batch_examples(starter, ender, model):
             num_batch += 1
             print("image number", num_img, " idx=", idx, "num_batch=", num_batch)
             correct_k += accuracy_float(output, target, topk=(1, 1))
-            if num_img >= args.predict_batch_nums*args.batch_size:
+            if num_img >= maximg:
                 is_stop = True
                 break
 
