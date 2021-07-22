12f8803fd59615a6eca59ab8fc8685ef0d26987c
nailixing <xingnaili14@gmail.com>
2021-07-22 11:27:27 +0800

    update col num

--- singa_easy/modules/mod_modelslicing/inference.py
+++ singa_easy/modules/mod_modelslicing/inference.py
@@ -316,7 +316,7 @@ def main():
 
     print("GPU-WARM-UP done")
 
-    log_test_1_batch_examples(model, starter, ender)
+    test_scheduler_batch_examples(model, starter, ender)
 
 
 def test_scheduler_batch_examples(starter, ender, model):
